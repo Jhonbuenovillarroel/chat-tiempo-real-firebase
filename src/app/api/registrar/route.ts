f879c89cee8d1488f2d5f1420c376b678288d4e4
Jhonbuenovillarroel <jhonadelbuenovillarroel@gmail.com>
2023-08-17 17:27:20 -0500

    Commit 8

--- src/app/api/registrar/route.ts
+++ src/app/api/registrar/route.ts
@@ -16,8 +16,10 @@ export async function POST(req: NextRequest) {
    });
 
    if (user) {
+      console.log(user);
       return NextResponse.json({ user: true });
    } else if (email) {
+      console.log(email);
       return NextResponse.json({ email: true });
    } else {
       const newUser = await prisma.user.create({
@@ -27,6 +29,7 @@ export async function POST(req: NextRequest) {
             password: body.password,
          },
       });
+      console.log(newUser);
 
       return NextResponse.json({ userCreated: true });
    }
