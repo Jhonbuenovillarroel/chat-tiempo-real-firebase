e9345c82dc87cf6814f5b7cb570ef4acd0b957f2
Jhonbuenovillarroel <jhonadelbuenovillarroel@gmail.com>
2023-08-18 17:16:58 -0500

    Commit 29

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,9 +2,10 @@
 
 import { escribirDatosUsuario, obtenerMensajes } from "@/lib/firebase-utils";
 import { useSession } from "next-auth/react";
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 import { onChildAdded, ref } from "firebase/database";
 import database from "@/lib/firebase";
+import { toast } from "react-hot-toast";
 
 interface Registro {
    message: string;
@@ -16,6 +17,7 @@ export default function Chat() {
    const { data: session, status } = useSession();
    const [messages, setMessages] = useState<Registro[]>([]);
    const [data, setData] = useState();
+   const ulRef = useRef<HTMLUListElement>(null);
 
    async function primeraObtencionMensajes() {
       const messagesDB: any = await obtenerMensajes();
@@ -31,7 +33,18 @@ export default function Chat() {
       const messagesDB: any = await obtenerMensajes();
 
       setMessages(messagesDB);
+
+      setMessageInput("");
+
+      console.log("si");
    });
+
+   useEffect(() => {
+      if (ulRef.current) {
+         window.scrollTo(0, 100000000);
+         ulRef.current.focus();
+      }
+   }, [messages]);
 
    useEffect(() => {
       primeraObtencionMensajes();
@@ -39,15 +52,18 @@ export default function Chat() {
 
    return (
       <main className="w-full">
-         <div className="w-full">
-            <ul className="flex w-full box-border p-8 mb-16 flex-col gap-4">
+         <div className="w-full mb-10">
+            <ul
+               ref={ulRef}
+               className="flex w-full box-border p-8 flex-col gap-4"
+            >
                {messages.length === 0 ? (
                   <div>Aún no hay mensajes</div>
                ) : (
                   messages.map((message: any, i) =>
                      message.username === session?.user?.name ? (
                         <li
-                           className="bg-white text-black max-w-[240px] justify-self-end self-end flex flex-col w-full p-4 rounded-md"
+                           className="bg-white text-black max-w-[240px] sm:max-w-lg justify-self-end self-end flex flex-col w-fit p-4 rounded-md"
                            key={`message_${i}`}
                         >
                            <span>{message.username}:</span>
@@ -55,10 +71,10 @@ export default function Chat() {
                         </li>
                      ) : (
                         <li
-                           className="bg-red-700 max-w-[240px] justify-self-end flex flex-col w-full p-4 rounded-md"
+                           className="bg-red-700 max-w-[240px] sm:max-w-lg justify-self-end flex flex-col w-fit p-4 rounded-md"
                            key={`message_${i}`}
                         >
-                           <span>{message.username}:</span>
+                           <span className="w-max">{message.username}:</span>
                            <p className="break-words">{message.message}</p>
                         </li>
                      )
@@ -69,6 +85,13 @@ export default function Chat() {
          <form
             onSubmit={async (e: FormEvent<HTMLFormElement>) => {
                e.preventDefault();
+
+               const formData = new FormData(e.currentTarget);
+
+               if (formData.get("message") === "") {
+                  toast.error("No puedes enviar texto vacío");
+                  return;
+               }
 
                if (messages) {
                   escribirDatosUsuario({
@@ -83,13 +106,13 @@ export default function Chat() {
                      message: messageInput,
                   });
                }
-
-               setMessageInput("");
             }}
             className="fixed bg-red-950 flex bottom-0 left-0 right-0"
             action=""
          >
             <input
+               id="message"
+               name="message"
                value={messageInput}
                onChange={(e: ChangeEvent<HTMLInputElement>) => {
                   setMessageInput(e.target.value);
